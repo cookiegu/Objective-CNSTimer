d656f32f1aff04cd187ffc500919ea08cfa98842
cookiegu <cookiegu.se@gmail.com>
2016-01-15 22:29:37 +0900

    init project

---
+++ OneTimeTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OneTimeTimer
+//
+//  Created by Cookie Gu on 16/1/15.
+//  Copyright © 2016年 Cookie Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OneTimeTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OneTimeTimer
+//
+//  Created by Cookie Gu on 16/1/15.
+//  Copyright © 2016年 Cookie Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
